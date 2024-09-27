322cfe386420fc6debcce0bd9ed48ae3b46300ac
Jared Cone <jared.cone@gmail.com>
2024-09-26 23:26:18 -0700

    Updated script to build the roc app

--- build-app.sh
+++ build-app.sh
@@ -8,17 +8,17 @@ HOST_BIN_SOURCE="$TARGET_FOLDER/host"
 HOST_LIB_SOURCE="$TARGET_FOLDER/libhost.a"
 HOST_LIB_DEST="platform/linux-x64.a"
 
-# build the stub
+# Build the stub lib.
 roc build --lib "$APP_ROC" --output "$STUB_LIB" --optimize
 
-# build the platform
+# Build the platform.
 cargo build --$CONFIG
 
-# copy the host binary
-# TODO is this necessary? Nothing seems to complain if we skip this step
+# Copy the host binary. This is only necessary for legacy linker?
 cp "$HOST_LIB_SOURCE" "$HOST_LIB_DEST"
 
-# surgical linker
+# Surgical linker.
 roc preprocess-host "$HOST_BIN_SOURCE" "$APP_ROC" "$STUB_LIB"
 
-#roc build --linker=legacy --prebuilt-platform "$APP_ROC"
+# Build the app
+roc build app/test.roc
