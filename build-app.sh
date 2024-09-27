37f24d2b4da5ad0e2dfc32e3115093ed38b064fe
Luke Boswell <lukewilliamboswell@gmail.com>
2024-09-27 17:54:21 +1000

    get the platform working

--- build-app.sh
+++ build-app.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 APP_ROC="app/test.roc"
-STUB_LIB="platform/libapp.so"
 CONFIG="release"
 TARGET_FOLDER="target/$CONFIG"
-HOST_BIN_SOURCE="$TARGET_FOLDER/host"
 HOST_LIB_SOURCE="$TARGET_FOLDER/libhost.a"
 HOST_LIB_DEST="platform/linux-x64.a"
-
-# Build the stub lib.
-roc build --lib "$APP_ROC" --output "$STUB_LIB" --optimize
 
 # Build the platform.
 cargo build --$CONFIG
--- platform/crates/host/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-
-    std::process::exit(host::rust_main());
-}
